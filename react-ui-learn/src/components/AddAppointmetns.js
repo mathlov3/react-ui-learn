e804d87f60e92e88f1f28478132b14267bfeba45
Yevhen_Slyva <yevhen_slyva@epam.com>
2019-08-12 22:43:14 +0300

    Add component structure

---
+++ react-ui-learn/src/components/AddAppointmetns.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class AddAppointments extends Component {
+    render () {
+        return <div>Add Appointmets</div>;
+    }
+}
+
+export default AddAppointments;
---
+++ react-ui-learn/src/components/ListAppointments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ListAppointmens extends Component {
+    render() {
+        return <div>List Appointments</div>;
+    }
+}
+
+export default ListAppointmens;
