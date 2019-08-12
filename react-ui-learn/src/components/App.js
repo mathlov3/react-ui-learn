3393274ec3477ff40993a131a83484f7a5de5928
Yevhen_Slyva <yevhen_slyva@epam.com>
2019-08-12 23:28:58 +0300

    Add lsit appointment data, view, remove handler

--- react-ui-learn/src/components/App.js
+++ react-ui-learn/src/components/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
+
+import { without } from 'lodash'
 
 import AddAppointments from './AddAppointmetns';
 import ListAppointments from './ListAppointments';
 import SearchAppointsment from './SearchAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointsment />
-              <ListAppointments />
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Yevhen',
+      myAppointments: [],
+      lastIndex: 0
+    }
+
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+  }
+
+  deleteAppointment(item) {
+    var tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, item);
+
+    this.setState({ myAppointments: tempApts });
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        })
+      });
+  }
+
+  render() {
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <AddAppointments />
+                <SearchAppointsment />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
